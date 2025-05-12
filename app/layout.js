c6db02d8936af8ff42870030cb5b10c617eb5487
scraper <155395474+datastatz@users.noreply.github.com>
2025-05-12 22:38:56 +0200

    Add Vercel Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const geistSans = Geist({
@@ -29,6 +30,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights/>
+
       </body>
     </html>
   );
